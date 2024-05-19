4ff5aece5095899834f53fc4f30ff0fa7fe510c1
Andrey <aa.kaznach@gmail.com>
2024-05-19 12:07:03 +0300

    Version 0.2.0
    - Create Logout
    - Create Profile

--- OrdersAppServer/main_server.py
+++ OrdersAppServer/main_server.py
@@ -1,4 +1,4 @@
-from flask import Flask, make_response, request
+from flask import Flask, make_response, request, jsonify
 from flask_restful import Resource, Api, reqparse
 import psycopg2 as pgdb
 import jwt
@@ -10,7 +10,6 @@ app = Flask(__name__)
 api = Api()
 
 secret_key = "ORDERS_APP_TOKEN_KEY"
-
 
 class UsersRegistration(Resource):
     def post(self):
@@ -46,6 +45,91 @@ class UsersRegistration(Resource):
         except Exception as e:
             return make_response({"error": str(e)}, 400)
 
+class UsersProfileData(Resource):
+    def put(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("profileNameDB", type=str, dest='company_name')
+        parser.add_argument("profilePhoneDB", type=str, dest='phone_num')
+        parser.add_argument("profileINNDB", type=str, dest='inn')
+        parser.add_argument("profileRegionDB", type=str, dest='region')
+        parser.add_argument("profileCityDB", type=str, dest='city')
+        parser.add_argument("profileSpecializationDB", type=str, dest='category')
+        args = parser.parse_args()
+
+        # Удаляем аргументы с пустыми значениями
+        args = {k: v for k, v in args.items() if v is not None}
+
+        token = request.headers.get('Authorization')
+
+        if not token:
+            return make_response({"error": "Token not provided"}, 401)
+
+        try:
+            token = token.split(" ")[1]
+            payload = jwt.decode(token, secret_key, algorithms=["HS256"])
+            user_id = payload['user_id']
+
+            cursor = conn.cursor()
+
+            # Динамически формируем SQL-запрос
+            set_clause = ", ".join([f"{key} = %s" for key in args])
+            update_query = f"""
+                UPDATE users
+                SET {set_clause}
+                WHERE id = %s
+            """
+            values = list(args.values()) + [user_id]
+            cursor.execute(update_query, values)
+
+            conn.commit()
+            cursor.close()
+
+            return make_response({"message": "Данные профиля успешно обновлены"}, 200)
+
+        except (jwt.ExpiredSignatureError, jwt.InvalidTokenError):
+            return make_response({"error": "Invalid or expired token"}, 401)
+        except Exception as e:
+            return make_response({"error": str(e)}, 400)
+
+    def get(self):
+        token = request.headers.get('Authorization')
+
+        if not token:
+            return make_response({"message": "Token not provided"}, 401)
+
+        try:
+            token = token.split(" ")[1]
+            payload = jwt.decode(token, secret_key, algorithms=["HS256"])
+            user_id = payload['user_id']
+
+            cursor = conn.cursor()
+            select_query = """
+                SELECT email, company_name, phone_num, inn, region, city, category
+                FROM users
+                WHERE id = %s
+            """
+            cursor.execute(select_query, (user_id,))
+            user_data = cursor.fetchone()
+            cursor.close()
+
+            if user_data:
+                email, company_name, phone_num, inn, region, city, category = user_data
+                return jsonify({
+                    "email": email,
+                    "company_name": company_name,
+                    "phone_num": phone_num,
+                    "inn": inn,
+                    "region": region,
+                    "city": city,
+                    "category": category
+                })
+            else:
+                return make_response({"message": "User not found"}, 404)
+
+        except (jwt.ExpiredSignatureError, jwt.InvalidTokenError):
+            return make_response({"message": "Invalid or expired token"}, 401)
+        except Exception as e:
+            return make_response({"error": str(e)}, 400)
 
 class UsersLogin(Resource):
     def post(self):
@@ -109,7 +193,6 @@ class UsersLogin(Resource):
         except Exception as e:
             return make_response({"error": str(e)}, 400)
 
-
 class CheckSession(Resource):
 
     def get(self):
@@ -141,6 +224,37 @@ class CheckSession(Resource):
             except Exception as e:
                 return make_response({"error": str(e)}, 400)
 
+class UsersLogout(Resource):
+
+    def put(self):
+
+        def close_session(token):
+            try:
+                cursor = conn.cursor()
+                query = """
+                    UPDATE sessions SET out_date = CURRENT_TIMESTAMP 
+                    WHERE token = %s 
+                    AND out_date IS NULL;
+                """
+                cursor.execute(query, (token,))
+                conn.commit()
+                cursor.close()
+            except Exception as e:
+                return False
+
+        token = request.headers.get('Authorization')
+        if not token:
+            return make_response({"message": "Невалидный токен"}, 401)
+        else:
+            try:
+                token = token.split(" ")[1]
+                if close_session(token):
+                    return make_response({"message": "Сессия завершена"}, 200)
+                else:
+                    return make_response({"message": "Сессия не завершена"}, 401)
+            except Exception as e:
+                return make_response({"error": str(e)}, 400)
+
 class CheckConnection(Resource):
     def head(self):
         try:
@@ -150,7 +264,9 @@ class CheckConnection(Resource):
 
 
 api.add_resource(UsersRegistration, "/api/v1/users/reg")
+api.add_resource(UsersProfileData, "/api/v1/users/profile")
 api.add_resource(UsersLogin, "/api/v1/users/log")
+api.add_resource(UsersLogout, "/api/v1/users/logout")
 api.add_resource(CheckSession, "/api/v1/sessions/check")
 api.add_resource(CheckConnection, "/api/v1/connection/check")
 api.init_app(app)
